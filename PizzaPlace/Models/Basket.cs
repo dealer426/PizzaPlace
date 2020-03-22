57b751b090a55b1bde42d2ff05aed496557d9a6f
dealer426 <burnssamuel01@gmail.com>
2020-03-22 10:50:47 -0400

    Added Basket class

---
+++ PizzaPlace/Models/Basket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Models
+{
+    public class Basket
+    {
+        public Customer Customer { get; set; } = new Customer();
+        public List<int> Orders { get; set; } = new List<int>();
+        public bool HasPaid { get; set; } = false;
+
+    }
+}
