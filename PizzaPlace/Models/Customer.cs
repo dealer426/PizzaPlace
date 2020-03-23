8758f3b44c5bc1827af3ddf6050d9a058dd2f4a0
dealer426 <burnssamuel01@gmail.com>
2020-03-22 20:19:46 -0400

    Added details to the form

--- PizzaPlace/Models/Customer.cs
+++ PizzaPlace/Models/Customer.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace PizzaPlace.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Street is required.")]
         public string Street { get; set; }
+        [Required(ErrorMessage = "City is required.")]
         public string City { get; set; }
 
         public Customer()
---
+++ PizzaPlace/Models/INotifyDataErrorInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Models
+{
+    public interface INotifyDataErrorInfo
+    {
+        bool HasErrors { get; }
+        event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        IEnumerable GetErrors(string propertyName);
+    }
+}
